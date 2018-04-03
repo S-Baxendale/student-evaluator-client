2d21dc773c827556cb5f308fcae97d362bce29d8
Scott Baxendale <scottbaxendale@hotmail.co.uk>
2018-04-03 19:07:10 +0200

    Handled FETCHED_ALL_BATCHES action type in batches reducer

---
+++ src/reducers/batches.js
@@ -0,0 +1,10 @@
+import { FETCHED_ALL_BATCHES } from '../actions/batches'
+
+export default function (state=[], action) {
+  switch(action.type) {
+    case FETCHED_ALL_BATCHES:
+      return action.payload
+    default:
+      return state
+  }
+}
