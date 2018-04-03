ce7ef10e02530cbfdc74f9de2f562de19aebf075
Scott Baxendale <scottbaxendale@hotmail.co.uk>
2018-04-03 19:56:07 +0200

    Handled CREATED_BATCH action type in batches reducer. Need to fix students.length render bug

--- src/reducers/batches.js
+++ src/reducers/batches.js
@@ -1,9 +1,11 @@
-import { FETCHED_ALL_BATCHES } from '../actions/batches'
+import { FETCHED_ALL_BATCHES, CREATED_BATCH } from '../actions/batches'
 
 export default function (state=[], action) {
   switch(action.type) {
     case FETCHED_ALL_BATCHES:
       return action.payload
+    case CREATED_BATCH:
+      return [...state, action.payload]
     default:
       return state
   }
