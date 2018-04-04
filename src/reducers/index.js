ccc1703094a1a8b3be7e860bf65183e56cf684f4
Scott Baxendale <scottbaxendale@hotmail.co.uk>
2018-04-04 16:28:48 +0200

    Handled CREATED_STUDENT action type in student reducer. Imported into root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
+import login from './login'
 import batches from './batches'
 import batch from './batch'
-import login from './login'
+import student from './student'
 
 export default {
   batches,
   batch,
+  student,
   currentUser: login
 
 }
