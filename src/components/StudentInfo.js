2a6b7049778c5399b20feb2e03530deb2ca5881a
Scott Baxendale <scottbaxendale@hotmail.co.uk>
2018-04-04 16:17:57 +0200

    Created empty StudentInfo component

---
+++ src/components/StudentInfo.js
@@ -0,0 +1,16 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+
+
+class StudentInfo extends PureComponent {
+
+  render() {
+
+    return(
+      <div>
+      </div>
+    )
+  }
+}
+
+export default StudentInfo
