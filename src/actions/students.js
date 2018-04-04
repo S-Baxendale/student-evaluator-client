7d935a015502f1fa7b37210cdc76c7643585d5a9
Scott Baxendale <scottbaxendale@hotmail.co.uk>
2018-04-04 15:49:53 +0200

    Corrected createAction API request to take Batch ID

--- src/actions/students.js
+++ src/actions/students.js
@@ -4,9 +4,9 @@ const baseUrl = 'http://localhost:4008'
 
 export const CREATED_STUDENT = 'CREATED_STUDENT'
 
-export const createStudent = (student) => (dispatch, getState) => {
+export const createStudent = (student, batchId) => (dispatch, getState) => {
   request
-    .post(`${baseUrl}/batches`)
+    .post(`${baseUrl}batches/${batchId}/students`)
     .send(student)
     .then(response => dispatch({
       type: CREATED_STUDENT,
