4ed62d125ee63e4a55e9da559746727d1358ca1c
Scott Baxendale <scottbaxendale@hotmail.co.uk>
2018-04-04 12:23:04 +0200

    Rendered array of Student components in StudentList

---
+++ src/components/Student.js
@@ -0,0 +1,18 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import '../styles/Student.css'
+
+class Student extends PureComponent {
+
+  render() {
+    return(
+      <div>
+      <h3>{this.props.firstName} {this.props.lastName}</h3>
+      <img src={this.props.photo} alt="Profile"/>
+      <p>Last evaluation: {this.props.evaluation}</p>
+      </div>
+    )
+  }
+}
+
+export default Student
--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -3,12 +3,22 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { fetchBatch } from '../actions/batches'
 
+import Student from './Student'
+import '../styles/Student.css'
+
 class StudentList extends PureComponent {
+  static propTypes = {
+    batch: PropTypes.shape({
+      id: PropTypes.number.isrequired,
+      startDate: PropTypes.string.isRequired,
+      endDate: PropTypes.string.isRequired,
+      students: PropTypes.array.isRequired
+    }).isRequired
+  }
 
   componentDidMount(props) {
     this.props.fetchBatch(this.props.match.params.id)
   }
-
 
   render() {
 
@@ -18,6 +28,21 @@ class StudentList extends PureComponent {
       <div>
         <h1>Batch #{batch.id}</h1>
 
+        <div className="flex-container">
+
+          {
+            batch.students.map((student, index) => (
+              <Student
+                className="flex"
+                key={index}
+                firstName={student.firstName}
+                lastName={student.lastName}
+                photo={student.photo}
+               />
+            ))
+          }
+
+        </div>
 
       </div>
     )
