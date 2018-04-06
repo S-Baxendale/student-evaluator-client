36e2b80c0f0e54771e93d8a6cd2d194caca81b2d
Scott Baxendale <scottbaxendale@hotmail.co.uk>
2018-04-06 16:01:04 +0200

    Send JWT in header for student actions

--- src/actions/students.js
+++ src/actions/students.js
@@ -8,8 +8,11 @@ export const REMOVED_STUDENT = 'REMOVED_STUDENT'
 export const UPDATED_STUDENT = 'UPDATED_STUDENT'
 
 export const createStudent = (student, batchId) => (dispatch, getState) => {
+  const state = getState()
+  const jwt = state.currentUser.jwt
   request
     .post(`${baseUrl}/batches/${batchId}/students`)
+    .set('Authorization', `Bearer ${jwt}`)
     .send(student)
     .then(response => dispatch({
       type: CREATED_STUDENT,
@@ -17,9 +20,12 @@ export const createStudent = (student, batchId) => (dispatch, getState) => {
     }))
 }
 
-export const fetchStudent = (studentId) => (dispatch) => {
+export const fetchStudent = (studentId) => (dispatch, getState) => {
+  const state = getState()
+  const jwt = state.currentUser.jwt
   request
       .get(`${baseUrl}/students/${studentId}`)
+      .set('Authorization', `Bearer ${jwt}`)
       .then(response => dispatch({
         type: FETCHED_STUDENT,
         payload: response.body
@@ -28,8 +34,11 @@ export const fetchStudent = (studentId) => (dispatch) => {
   }
 
 
-export const removeStudent = (studentId) => (dispatch) => {
+export const removeStudent = (studentId) => (dispatch, getState) => {
+  const state = getState()
+  const jwt = state.currentUser.jwt
   request
+    .set('Authorization', `Bearer ${jwt}`)
     .delete(`${baseUrl}/students/${studentId}`)
     .then(response => dispatch({
       type: REMOVED_STUDENT,
@@ -38,9 +47,12 @@ export const removeStudent = (studentId) => (dispatch) => {
 
 }
 
-export const updateStudent = (studentId, updates) => (dispatch) => {
+export const updateStudent = (studentId, updates) => (dispatch, getState) => {
+  const state = getState()
+  const jwt = state.currentUser.jwt
   request
     .put(`${baseUrl}/students/${studentId}`)
+    .set('Authorization', `Bearer ${jwt}`)
     .send(updates)
     .then(response => dispatch ({
       type: UPDATED_STUDENT,
